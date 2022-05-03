de9769b9438570e16acaa139a1f0b56fd9fb9031
A. Bernstein <a.bernstein@sprinteins.com>
2022-05-03 19:12:03 +0200

    Add quiz UI
    The quizzes are listed under lessons. After click the user is navigated to the Topic subpage.

--- src/api/quiz/controllers/quiz.js
+++ src/api/quiz/controllers/quiz.js
@@ -1,9 +1,24 @@
-'use strict';
+'use strict'
 
 /**
  *  quiz controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require('@strapi/strapi').factories
 
-module.exports = createCoreController('api::quiz.quiz');
+module.exports = createCoreController('api::quiz.quiz', ({ strapi }) => ({
+  async findOne(ctx) {
+    let { data, meta } = await super.findOne(ctx)
+
+    // Remove private field State from answer attribute of a quiz response
+    if (data.attributes.answers && data.attributes.answers.length > 0) {
+      for (let answer of data.attributes.answers) {
+        // Check for private field State in an answer. It could be fixed later by strapi team.
+        if ('State' in answer) delete answer['State']
+      }
+    }
+    delete data.attributes.lesson
+
+    return { data, meta }
+  }
+}))
